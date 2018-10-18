53ac7e0cf6f8548f43174a7a5ba8d16525ffbb39
Jim Was Here <43002597+r3dxpl0it@users.noreply.github.com>
2018-10-18 23:06:51 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+'''_____________________________________________________________________
+|[] R3DXPL0IT SHELL                                            |ROOT]|!"|
+|"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""|"| 
+|CODED BY > R3DXPLOIT(JIMMY)                                          | |
+|EMAIL > RETURN_ROOT@PROTONMAIL.COM                                   | |
+|GITHUB > https://github.com/r3dxpl0it                                | |
+|WEB-PAGE > https://r3dxpl0it.Github.io                               |_|
+|_____________________________________________________________________|/|
+'''
+
+#CONFIG
+user_agents = ['Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36' ,
+'Roku4640X/DVP-7.70 (297.70E04154A)' ,
+'Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Safari/534.30' , 
+'Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus Player Build/MMB29T)' , 
+'Mozilla/5.0 (Linux; Android 5.1; AFTS Build/LMY47O) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/41.99900.2250.0242 Safari/537.36]']
+
+intels = ['github.com', 'facebook.com', 'instagram.com', 'youtube.com']
+badTypes = ['png', 'jpg', 'jpeg', 'pdf', 'js', 'css', 'ico', 'bmp', 'svg', 'json', 'xml', 'xls', 'csv', 'docx']
